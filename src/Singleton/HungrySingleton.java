8aaf98e2384a759a4ca9e1091d1be2105a707bce
yuwell1999 <yuwell1999@users.noreply.github.com>
2020-08-22 16:40:18 +0800

    新增 单例模式 demo

---
+++ src/Singleton/HungrySingleton.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class HungrySingleton {
+    private static HungrySingleton instance = new HungrySingleton();
+
+    private HungrySingleton() {}
+
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
+}
